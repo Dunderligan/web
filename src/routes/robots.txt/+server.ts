72ea217faebaa8460adc98808bd040478d7af905
Bo Rodin <bobbo.nidor@gmail.com>
2026-02-25 16:48:14 +0100

    Forbid crawling on dev

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,12 @@
+import { redirect, type RequestHandler } from '@sveltejs/kit';
+import robotsTxtDev from '$lib/assets/robots-dev.txt';
+import robotsTxtProd from '$lib/assets/robots-prod.txt';
+import { env } from '$env/dynamic/private';
+
+export const GET: RequestHandler = async () => {
+	if (env.FORBID_CRAWLING === 'true') {
+		return redirect(303, robotsTxtDev);
+	} else {
+		return redirect(303, robotsTxtProd);
+	}
+};
