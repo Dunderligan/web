c8e8f4720e05d496acd7d2d3519b37662c248714
Bo Rodin <bobbo.nidor@gmail.com>
2026-03-04 15:45:42 +0100

    Fix merge conflict

--- src/routes/(app)/stallningar/[season]/+page.svelte
+++ src/routes/(app)/stallningar/[season]/+page.svelte
@@ -144,32 +144,17 @@
 </PageHeader>
 
 <PageSection>
-	<div class="mb-4 max-w-xl space-y-1.5">
-		{#if divisions.length > 1}
-			<Tabs
-				selected={division.id}
-				items={divisions.map((division) => ({
-					label: division.name,
-					value: division.id,
-					href: `?div=${division.slug}&visa=${mode === 'group' ? 'gruppspel' : 'slutspel'}`
-				}))}
-			/>
-		{/if}
-
-		<Tabs fillIcons selected={mode} items={tabItems} />
-	</div>
-
-	{#if mode === 'group'}
-		{#each division.tables as table}
-			{@const resolvedStandings = table.standings.map(({ rosterId, score }) => ({
-				roster: resolveRoster(rosterId)!,
-				score
-			}))}
-
-			{#if table.type === 'grupp'}
-				<Subheading class="mt-6 mb-4 flex justify-between">
-					{table.title}
-				</Subheading>
+	<section class="grow overflow-hidden">
+		<div class="mb-4 max-w-xl space-y-1.5">
+			{#if divisions.length > 1}
+				<Tabs
+					selected={division.id}
+					items={divisions.map((division) => ({
+						label: division.name,
+						value: division.id,
+						href: `?div=${division.slug}&visa=${mode === 'group' ? 'gruppspel' : 'slutspel'}`
+					}))}
+				/>
 			{/if}
 
 			<Tabs fillIcons selected={mode} items={tabItems} />
@@ -209,18 +194,34 @@
 				title="Kommande matcher"
 				matches={division.upcomingMatches.map(resolveMatch)}
 				seasonSlug={season.slug}
+				class="mt-12"
+				hideIfEmpty
 			/>
 
-			{#if page.data.user?.isAdmin}
-				<Button
-					label="Redigera"
-					icon="ph:pencil-simple"
-					kind="secondary"
-					class="mt-4 max-w-max"
-					href="/admin/{table.type}/{table.id}"
-				/>
+			{#if division.latestMatches.length > 0}
+				<div class="mt-8 mb-4 flex max-w-2xl items-center justify-between">
+					<Subheading>Senaste matcherna</Subheading>
+
+					<Button
+						href="/arkiv/matcher?division={division.id}&spelad=true&prev={page.url.pathname}"
+						label="Se alla"
+						icon="ph:arrow-right"
+						kind="secondary"
+					/>
+				</div>
+
+				<MatchList matches={division.latestMatches.map(resolveMatch)} seasonSlug={season.slug} />
 			{/if}
-		{/each}
+		{:else}
+			{#each division.brackets as bracket (bracket.id)}
+				{#if division.brackets.length > 1 || bracket.name !== division.name}
+					<Subheading class="mt-6 mb-4">{bracket.name}</Subheading>
+				{/if}
+
+				<Bracket
+					seasonSlug={season.slug}
+					rounds={buildBracketRounds(bracket.matches.map(resolveMatch))}
+				/>
 
 				{#if hasPermission(page.data.user?.role, AuthRole.ADMIN)}
 					<Button
@@ -233,26 +234,5 @@
 				{/if}
 			{/each}
 		{/if}
-	{:else}
-		{#each division.brackets as bracket (bracket.id)}
-			{#if division.brackets.length > 1 || bracket.name !== division.name}
-				<Subheading class="mt-6 mb-4">{bracket.name}</Subheading>
-			{/if}
-
-			<Bracket
-				seasonSlug={season.slug}
-				rounds={buildBracketRounds(bracket.matches.map(resolveMatch))}
-			/>
-
-			{#if page.data.user?.isAdmin}
-				<Button
-					label="Redigera"
-					icon="ph:pencil-simple"
-					kind="secondary"
-					class="mt-4 max-w-max"
-					href="/admin/bracket/{bracket.id}"
-				/>
-			{/if}
-		{/each}
-	{/if}
+	</section>
 </PageSection>
