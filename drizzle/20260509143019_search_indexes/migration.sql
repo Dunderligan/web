e383ee77e2a28c3a2e55da5ecfc1edf04f6bdaf2
Kesomannen <bobbo.nidor@gmail.com>
2026-05-09 16:31:30 +0200

    Revert drizzle and add search index migration

---
+++ drizzle/20260509143019_search_indexes/migration.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "match" ADD COLUMN "round" integer;--> statement-breakpoint
+CREATE INDEX "division_name_gin_idx" ON "division" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "group_name_gin_idx" ON "group" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "player_battletag_gin_idx" ON "player" USING gin ("battletag" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "player_alias_name_gin_idx" ON "player_alias" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "roster_name_gin_idx" ON "roster" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
+CREATE INDEX "season_name_gin_idx" ON "season" USING gin ("name" gin_trgm_ops);
