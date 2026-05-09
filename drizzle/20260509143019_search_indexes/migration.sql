b00a053604bd3b7f333030987138669e4ba98ad7
Kesomannen <bobbo.nidor@gmail.com>
2026-05-09 16:44:42 +0200

    Add extension to migration

--- drizzle/20260509143019_search_indexes/migration.sql
+++ drizzle/20260509143019_search_indexes/migration.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
 CREATE INDEX "division_name_gin_idx" ON "division" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
 CREATE INDEX "group_name_gin_idx" ON "group" USING gin ("name" gin_trgm_ops);--> statement-breakpoint
 CREATE INDEX "player_battletag_gin_idx" ON "player" USING gin ("battletag" gin_trgm_ops);--> statement-breakpoint
